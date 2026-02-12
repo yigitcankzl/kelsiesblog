80f650cd0ba8c18fdc832aa9210022b0663abfd0
Yiğitcan Kızıl <kizilyigit33@hotmail.com>
2026-02-13 01:13:10 +0300

    Refactor: Extract application layout and routing into a new `AppContent` component to utilize `useLocation`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import MapPage from './pages/MapPage';
 import AdminPage from './pages/AdminPage';
 
+function AppContent() {
+  const location = useLocation();
+  const isAdmin = location.pathname === '/admin';
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MapPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </div>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex flex-col h-full">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<MapPage />} />
-          <Route path="/admin" element={<AdminPage />} />
-        </Routes>
-      </div>
+      <AppContent />
     </BrowserRouter>
   );
 }
